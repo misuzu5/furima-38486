2a95374bb486eb26a36178703c3cbf94a72bd386
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-10 18:26:19 +0900

    名前全角設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_day, presence: true
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name, presence: true
+    validates :first_name, presence: true
+    validates :family_name_kana, presence: true
+    validates :first_name_kana, presence: true  
+  end
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+  
 end
