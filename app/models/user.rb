f8747d65580694cc9cf145679812c59697e53f3f
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-11 17:24:01 +0900

    presence: true　削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,13 @@ class User < ApplicationRecord
   validates :birth_day, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-    validates :family_name, presence: true
-    validates :first_name, presence: true
+    validates :family_name
+    validates :first_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: '全角カナを使用してください' } do
-    validates :family_name_kana, presence: true
-    validates :first_name_kana, presence: true
+    validates :family_name_kana
+    validates :first_name_kana
   end
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
