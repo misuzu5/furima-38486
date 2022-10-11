0208898956caf485e949ce84bb47079487b29fba
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-11 15:03:56 +0900

    全角かな/カナ漢字 正規表現に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birth_day, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :family_name, presence: true
     validates :first_name, presence: true
     validates :family_name_kana, presence: true
