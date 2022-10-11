019b04e3ee7619b57c0c9ea0ba978cf1ab5e00aa
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-11 09:36:28 +0900

    Gimeiで漢字カタカナ生成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,20 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.nickname}
+
+    japanese_user = Gimei.name
+    
+    nickname              { japanese_user.first.hiragana }
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    family_name           {Faker::Name.family_name}
-    first_name            {Faker::Name.first_name}
-    family_name_kana      {Faker::Name.family_name_kana}
-    first_name_kana       {Faker::Name.first_name_kana}
-    birth_day             {Faker::Date.birth_day}
+    family_name           { japanese_user.last.kanji }
+    first_name            { japanese_user.first.kanji }
+    family_name_kana      { japanese_user.last.katakana }
+    first_name_kana       { japanese_user.first.katakana }
+    birth_day             { Faker::Date.backward }
   end
 end
+
+# https://qiita.com/taka_no_okapi/items/2cd78d3168b90e5c2534
+# https://qiita.com/shun915a/items/c093559e7acdcf4cd060
+# https://github.com/willnet/gimei
