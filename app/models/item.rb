1f4b00862497c38a9446ae492b885efc0a89d3aa
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-12 15:40:39 +0900

    バリデーション追加編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,45 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_id
   belongs_to :status_id
   belongs_to :cost_id
   belongs_to :sender_id
   belongs_to :delivery_time_id
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :sender_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :cost_id
+    validates :sender_id
+    validates :delivery_time_id
+    validates :user_id
+    validates :image
+  end
 
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: "can't be blank" } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :cost_id
+    validates :sender_id
+    validates :delivery_time_id
+  end
 end
+
+# Railsのバリデーションの種類と使い方
+# https://qiita.com/mzmz__02/items/8f4b90e98712c76a031e
+
+# [Ruby on Rails] 金額のバリデーション(¥300~¥9,999,999) と テストコード
+# https://qiita.com/BeautifulDreamer/items/8f42cc6ff3ce0a44554d
+
+# Rubyのバリデーション用正規表現集
+# https://gist.github.com/nashirox/38323d5b51063ede1d41
