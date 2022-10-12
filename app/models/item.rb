ea86e5491d84cbae8f51735a54d1cbbe1896e03a
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-12 14:42:03 +0900

    ActiveHash作成、アソシエーション、バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :status_id
+  belongs_to :cost_id
+  belongs_to :sender_id
+  belongs_to :delivery_time_id
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sender_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
