b81af85231c39a49a8bd0c3f0ac2fcafb11aeeef
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-12 12:04:02 +0900

    ActiveStorage導入、アソシエーション、ストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,12 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :status_id, :cost_id, :sender_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
