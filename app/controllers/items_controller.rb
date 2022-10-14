7759be9aa4760541d302bcdf1ce0c7a33a84aba7
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-14 17:35:27 +0900

    set_current_userメソッド修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -53,6 +53,7 @@ class ItemsController < ApplicationController
 
   def set_current_user
     unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
     end
   end
 end
