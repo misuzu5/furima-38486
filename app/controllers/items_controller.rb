59a0d2e9401ae3e8ebf6c2161b59969e968002a1
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-14 13:46:11 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,9 +30,9 @@ class ItemsController < ApplicationController
   end
 
   def update  
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.save
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
       redirect_to item_path
     else
       render :edit
