3d5c974bcc5f5576246acc739aae1722b1684a5a
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-17 17:04:40 +0900

    商品購入ページへのアクセス可否

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,4 +1,6 @@
 class BuyersController < ApplicationController
+  before_action :sold_item, only: [:index, :create]
+
   def index
     @buyer_address = BuyerAddress.new
   end
@@ -17,5 +19,16 @@ class BuyersController < ApplicationController
 
   def buyer_params
     params.require(:buyer_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def sold_item
+    @item = Item.find(params[:item_id])
+    if user_signed_in?
+      redirect_to root_path
+    elsif current_user == @item.user_id || @item.buyer.nil?
+      redirect_to root_path
+    else @item.user_id || @item.buyer.nil?
+      redirect_to root_path
+    end
   end
 end
