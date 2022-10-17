c258e3071efa465e4de00ea88c1e4a51566e5ec9
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-17 11:40:28 +0900

    buyer_addressモデル名変更

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,4 +1,4 @@
-class BuyersAddress
+class BuyerAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
 
