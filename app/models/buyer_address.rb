78fe04a40efab31cc7e7a290ed1292e810df17a9
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-17 12:36:30 +0900

    postal_code→ post_code 修正

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,11 +1,11 @@
 class BuyerAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
     validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "is invalid"}
@@ -15,6 +15,6 @@ class BuyerAddress
 
   def save
     buyers = Buyers.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, buyer_id: buyer.id)
+    Address.create(post_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, buyer_id: buyer.id)
   end
 end
