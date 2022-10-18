a2ae5a6b4446268ad15ae19f296a618ce01f2afa
miho morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-18 15:10:13 +0900

    決済処理実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -9,6 +9,7 @@ class BuyersController < ApplicationController
   def create
     @buyer_address = BuyerAddress.new(buyer_params)
     if @buyer_address.valid?
+      pay_item
       @buyer_address.save
       return redirect_to root_path
     else
@@ -20,6 +21,15 @@ class BuyersController < ApplicationController
 
   def buyer_params
     params.require(:buyer_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_5ae5c5681b632d9bb65e697e"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buyer_params[:token],
+      currency: 'jpy'
+    )
   end
 
   def sold_item
